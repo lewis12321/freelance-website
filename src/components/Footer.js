4e73d593e1abce6f0b5cd2adaa19ef8cb34138c5
Lewis <lewis_jefferies@live.co.uk>
2018-07-05 07:20:12 +0100

    Added details to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,31 +6,26 @@ import logo from '../assets/images/logo.svg';
 const Footer = (props) => (
     <footer id="footer">
         <section>
-            <h2>Aliquam sed mauris</h2>
+            <h2>Work for you?</h2>
             <p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
             <ul className="actions">
                 <li><Link to="/generic" className="button">Learn More</Link></li>
             </ul>
         </section>
         <section>
-            <h2>Etiam feugiat</h2>
+            <h2>Contact me</h2>
             <dl className="alt">
-                <dt>Address</dt>
-                <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
                 <dt>Phone</dt>
-                <dd>(000) 000-0000 x 0000</dd>
+                <dd>07720 683615</dd>
                 <dt>Email</dt>
-                <dd><a href="#">information@untitled.tld</a></dd>
+                <dd><a href="mailto:ljefferies98@gmail.com">ljefferies98@gmail.com</a></dd>
             </dl>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook alt"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon fa-dribbble alt"><span className="label">Dribbble</span></a></li>
+                <li><a href="https://github.com/lewis12321" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
+                <li><a href="https://uk.linkedin.com/in/lewis-jefferies-323742147" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a></li>
             </ul>
         </section>
-        <p className="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <p className="copyright">&copy; Lewis Jefferies. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
     </footer>
 )
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>Lewis Jefferies</h1>
+        <p>A web developer & programmer based in Bristol, UK.</p>
     </header>
 )
 
